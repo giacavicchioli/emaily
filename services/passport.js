f268275c28c453bf3af511354038780092670d8a
Giacomo Cavicchioli <cavicchioli.giacomo@gmail.com>
2019-02-24 14:37:24 +0100

    created authentication with google oauth

---
+++ services/passport.js
@@ -0,0 +1,19 @@
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("../config/keys");
+
+/**
+ * Passport configuration for Google authentication
+ */
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback"
+    },
+    accessToken => {
+      console.log("accessToken:", accessToken);
+    }
+  )
+);
