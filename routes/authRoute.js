184d7c69eb1431ee202eb8dbf566516f78e81af2
Giacomo Cavicchioli <cavicchioli.giacomo@gmail.com>
2019-02-26 13:57:23 +0100

    implemented logout

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -13,9 +13,13 @@ module.exports = app => {
     passport.authenticate("google", { failureRedirect: "/login" }),
     function(req, res) {
       // Successful authentication, redirect home.
-      res.redirect("/");
     }
   );
+
+  app.get("/api/logout", (req, res) => {
+    req.logout();
+    res.send(req.user);
+  });
 
   app.get("/api/current_user", (req, res) => {
     res.send(req.user);
