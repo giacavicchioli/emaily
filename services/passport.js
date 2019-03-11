53eaef6efa5c06a765df0874293add39858666e7
Giacomo Cavicchioli <cavicchioli.giacomo@gmail.com>
2019-03-11 21:21:07 +0100

    reafactor googlestrategy callback with async/await

--- services/passport.js
+++ services/passport.js
@@ -26,15 +26,17 @@ passport.use(
       callbackURL: "/auth/google/callback",
       proxy: true
     },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id }).then(existingUser => {
-        if (existingUser) {
-          // we already have a record with the given profile ID
-          done(null, existingUser);
-        } else {
-          new User({ googleId: profile.id }).save().then(u => done(null, u));
-        }
-      });
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
+
+      if (existingUser) {
+        // we already have a record with the given profile ID
+        return done(null, existingUser);
+      }
+
+      // we don't have a user record with this ID, make a new record!
+      const u = await new User({ googleId: profile.id }).save();
+      done(null, u);
     }
   )
 );
