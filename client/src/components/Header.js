0579daa580f5f0e73b41f427d21295b064617fad
Giacomo Cavicchioli <cavicchioli.giacomo@gmail.com>
2019-03-11 22:13:07 +0100

    header ui

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,8 +1,26 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 class Header extends React.Component {
   render() {
-    return <div>Hello</div>;
+    return (
+      <nav>
+        <div className="nav-wrapper">
+          <Link to="/" className="brand-logo">
+            Emaily
+          </Link>
+          <ul id="nav-mobile" className="right hide-on-med-and-down">
+            <li>
+              <a href="/auth/google">Login with Google</a>
+            </li>
+            <li>
+              <Link to="/surveys" className="right hide-on-med-and-down">
+                Dashboard
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
   }
 }
 
