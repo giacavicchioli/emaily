3d4641f65f316a2951fbd949506029cc50fda8da
Giacomo Cavicchioli <cavicchioli.giacomo@gmail.com>
2019-03-11 21:47:43 +0100

    created basic routes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,25 @@
 import React, { Component } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
+import Header from "./Header";
+
+const Dashboard = () => <h2>Dashboard</h2>;
+const SourveyNew = () => <h2>SourveyNew</h2>;
+const Landing = () => <h2>Landing</h2>;
 
 const App = () => {
-  return <div>Hi there!</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/surveys" component={Dashboard} />
+          <Route exact path="/surveys/new" component={SourveyNew} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
