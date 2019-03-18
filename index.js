0e8a1ae871e12a8f079512401c948f5b75422bc8
Giacomo Cavicchioli <cavicchioli.giacomo@gmail.com>
2019-03-18 21:50:45 +0100

    fix port conflict

--- index.js
+++ index.js
@@ -28,5 +28,5 @@ app.use(passport.session());
 require("./routes/authRoute")(app);
 
 // listen on port
-const PORT = process.env.PORT || 5557;
+const PORT = process.env.PORT || 8181;
 app.listen(PORT);
