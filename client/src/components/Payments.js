ca23fdca92a551a835bbf5be5de6eaed818dd29a
Giacomo Cavicchioli <cavicchioli.giacomo@gmail.com>
2019-03-24 17:13:02 +0100

    changed stripe form details

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -9,10 +9,14 @@ class Payments extends React.Component {
   render() {
     return (
       <StripeCheckout
+        name="Emaily"
+        description="$5 for 5 email credits"
         token={this.onToken}
         amount={500}
         stripeKey={process.env.REACT_APP_STRIPE_KEY}
-      />
+      >
+        <button className="btn">Add credits</button>
+      </StripeCheckout>
     );
   }
 }
