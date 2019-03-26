db1cae053b535fac43fff5cbe373cc3665b9f131
Giacomo Cavicchioli <cavicchioli.giacomo@gmail.com>
2019-03-26 14:04:46 +0100

    created express routes for client

--- index.js
+++ index.js
@@ -30,6 +30,17 @@ app.use(passport.session());
 require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
 
+if (process.env.NODE_ENV === "production") {
+  // express will serve up production assets, like main.js
+  app.use(express.static("client/build"));
+
+  // express will serve up index.html if it doesn't recognize the route
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 // listen on port
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
