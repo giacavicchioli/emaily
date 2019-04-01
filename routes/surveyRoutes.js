92a92baed30daab43e0ea6e6f9b3085ecb3e5626
Giacomo Cavicchioli <cavicchioli.giacomo@gmail.com>
2019-04-01 22:07:13 +0200

    completed surveyRoute and added axios to window for debug

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -21,11 +21,23 @@ module.exports = app => {
       dateSent: Date.now()
     });
 
+    console.log(survey);
+
     // attempt to create and send email
     const mailer = new Mailer(survey, surveyTemplate(survey));
-    mailer.send();
 
-    // save survey
-    // survey.save();
+    try {
+      await mailer.send();
+
+      // await survey.save();
+      // req.user.credits -= 1;
+      const user = await req.user.save();
+
+      res.send(user);
+    } catch (err) {
+      res.status(422).send(err);
+      console.error(err);
+      console.error(err.response.body.errors);
+    }
   });
 };
