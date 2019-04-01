1fcf25f5ed5bf649bab5c059767c748efcb73518
Giacomo Cavicchioli <cavicchioli.giacomo@gmail.com>
2019-04-01 22:06:47 +0200

    fixed mailer

--- services/Mailer.js
+++ services/Mailer.js
@@ -9,7 +9,7 @@ class Mailer extends helper.Mail {
     this.sgApi = sendgrid(keys.sendGridKey);
     this.from_email = new helper.Email("no-reply@emaily.com");
     this.subject = subject;
-    this.body = new herlper.Context("text/html", content);
+    this.body = new helper.Content("text/html", content);
     this.recipients = this.formatAddresses(recipients);
 
     this.addContent(this.body);
@@ -42,12 +42,14 @@ class Mailer extends helper.Mail {
 
   async send() {
     const request = this.sgApi.emptyRequest({
-      methos: "POST",
+      method: "POST",
       path: "/v3/mail/send",
       body: this.toJSON()
     });
 
-    const response = this.sgApi.API(request);
+    const response = await this.sgApi.API(request);
+    console.log("sendgrid response: ", response);
+
     return response;
   }
 }
