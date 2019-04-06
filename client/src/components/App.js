a3b4a87e93c92c2172887782c0426098744af1b1
Giacomo Cavicchioli <cavicchioli.giacomo@gmail.com>
2019-04-06 11:52:34 +0200

    added add button to dashboard

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -18,7 +18,7 @@ class App extends React.Component {
     return (
       <div>
         <BrowserRouter>
-          <div>
+          <div className="container">
             <Header />
             <Route exact path="/" component={Landing} />
             <Route exact path="/surveys" component={Dashboard} />
