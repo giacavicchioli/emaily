31207f74831e176f5517a5e0c8f43ff55caecc27
Giacomo Cavicchioli <cavicchioli.giacomo@gmail.com>
2019-04-09 21:51:20 +0200

    added form review

--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -2,10 +2,25 @@
 
 import React from "react";
 import SurveyForm from "./SurveyForm";
+import SurveyFormReview from "./SurveyFormReview";
 
 class SurveyNew extends React.Component {
+  state = { showFormReview: false };
+
+  renderContent() {
+    if (this.state.showFormReview) {
+      return <SurveyFormReview />;
+    }
+
+    return (
+      <SurveyForm
+        onSurveySubmit={() => this.setState({ showFormReview: true })}
+      />
+    );
+  }
+
   render() {
-    return <SurveyForm />;
+    return <div>{this.renderContent()}</div>;
   }
 }
 
